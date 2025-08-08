86eb832821ea48a06bdd1d559b5c5c4a2acacf29
Arsiwar <arourisiwar57@gmail.com>
2025-08-08 15:28:12 +0200

    Modifications sur roomController.js sur la branche main

--- backend/src/Controllers/roomController.js
+++ backend/src/Controllers/roomController.js
@@ -4,7 +4,7 @@ const getRoomsByBuilding = async (req, res) => {
   try {
     const rooms = await Room.find({ buildingId: req.params.buildingId });
     // Force une erreur
-    throw new Error('Erreur test');
+    //throw new Error('Erreur test');
     res.json(rooms);
   } catch (err) {
     res.status(500).json({ message: 'Erreur serveur' });
