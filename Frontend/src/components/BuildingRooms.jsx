5e65b8366fbfab89c737137cdbd1b06d06c46179
Arsiwar <arourisiwar57@gmail.com>
2025-08-12 12:18:54 +0200

    Ajout des imports pour react-leaflet

--- Frontend/src/components/BuildingRooms.jsx
+++ Frontend/src/components/BuildingRooms.jsx
@@ -7,6 +7,15 @@ import { ArrowLeft } from 'lucide-react';
 import LogoutButton from './LogoutButton';
 import jsPDF from 'jspdf';
 import axios from 'axios';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'; // Vérifie cet import
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
 
 // Configuration des bâtiments
 const buildingsData = {
